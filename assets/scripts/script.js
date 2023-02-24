4610ad16fda56e442362b4b3433df15e036282e6
PatrykPorabik <patryk@porabik.pl>
2023-02-24 13:19:14 +0100

    added overlay, still fighting with margin

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menulist-mobile');
+const overlay = document.querySelector('.overlay');
 
 const handleClick = () => {
     hamburger.classList.toggle('hamburger--active');
     menu.classList.toggle('menulist-mobile--active');
+    overlay.classList.toggle('overlay--active');
 }
 
 hamburger.addEventListener('click', handleClick);
