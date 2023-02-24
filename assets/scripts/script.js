be1c1b077e4b0b2a46e8e65731588ff0be6085b1
PatrykPorabik <patryk@porabik.pl>
2023-02-24 09:41:27 +0100

    added mobile menu, 1 problem left - hidden menu still visible out of the screen

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menulist');
 
 const handleClick = () => {
     hamburger.classList.toggle('hamburger--active');
+    menu.classList.toggle('menulist--active');
 }
 
 hamburger.addEventListener('click', handleClick);
