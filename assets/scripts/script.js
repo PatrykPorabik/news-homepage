99f697d96337a6e81b9d63039af6ccf51315d3ed
PatrykPorabik <patryk@porabik.pl>
2023-02-24 10:50:09 +0100

    last touches on menu

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,9 @@
 const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.menulist');
+const menu = document.querySelector('.menulist-mobile');
 
 const handleClick = () => {
     hamburger.classList.toggle('hamburger--active');
-    menu.classList.toggle('menulist--active');
+    menu.classList.toggle('menulist-mobile--active');
 }
 
 hamburger.addEventListener('click', handleClick);
